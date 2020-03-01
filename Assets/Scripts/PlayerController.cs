994f8f02dd0847f88952357d8f9783cb025801b0
ERmilburn02 <61363431+ERmilburn02@users.noreply.github.com>
2020-03-01 01:13:05 +0000

    ...

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    private int extraJumps;
+    public int extraJumpValue;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        extraJumps = extraJumpValue;
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("Jump") == 1 && extraJumps > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+        else if (Input.GetAxis("Jump") == 1 && extraJumps == 0 && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+        if (isGrounded == true)
+        {
+            extraJumps = extraJumpValue;
+        }
+    }
+    void FixedUpdate()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+    }
+}
